32e898a80add273eb218d049fec278bec8fe2779
Arath García <arathgg143@gmail.com>
2023-02-22 23:34:18 -0600

    aggregate cistern measurements

--- arduino_firebase.ino
+++ arduino_firebase.ino
@@ -2,8 +2,14 @@
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 
-#define WIFI_SSID "Megacable_2.4G_DFCE"
-#define WIFI_PASSWORD "TAxKA2ES"
+//#define WIFI_SSID "iPhoneX"
+//#define WIFI_PASSWORD "01234567"
+
+#define WIFI_SSID "Megcable_2.4G_96B4"
+#define WIFI_PASSWORD "jackito11"
+
+//#define WIFI_SSID "UlisesF"
+//#define WIFI_PASSWORD "z28s5jww"
 
 #define FIREBASE_HOST "fir-tam-arduino-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "EseNGxrWBBmZd9ydPKWSae6Bh4DPQULxU67RFtW1"
@@ -12,7 +18,6 @@ FirebaseData firebaseData;
 
 // Define maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500 cm:
 #define MAX_DISTANCE 400
-#define pinPot 33
 
 #define trigCis_1A 19
 #define echoCis_1A 18
@@ -21,18 +26,12 @@ FirebaseData firebaseData;
 #define trigCis_3A 33
 #define echoCis_3A 32
 
-int levelPot = 0; // for testing only
-String nodo = "/";
-bool iterar = true;
-
 // NewPing setup of pins and maximum distance.
 NewPing cisterna_1A = NewPing(trigCis_1A, echoCis_1A, MAX_DISTANCE);
 NewPing cisterna_2A = NewPing(trigCis_2A, echoCis_2A, MAX_DISTANCE);
 NewPing cisterna_3A = NewPing(trigCis_3A, echoCis_3A, MAX_DISTANCE);
 
-int distanciaSA1 = 0;
-float ancho = 3;
-float largo = 3;
+String nodo = "/";
 
 void setup() {
   
@@ -50,7 +49,6 @@ void setup() {
     
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-  pinMode(pinPot, INPUT);
 
 }
 
@@ -58,19 +56,20 @@ void loop() {
   delay(1000);
  
   leerCisterna("cisterna1", cisterna_1A);
-  leerCisterna("cisterna2", cisterna_2A);
-  leerCisterna("cisterna3", cisterna_3A);
 }
 
 void leerCisterna(const char* nombre, NewPing cisterna){
   float valor_distancia_1 = cisterna.ping_cm();
   float valor_distancia = valor_distancia_1;
+  float valorPorcentaje = map(valor_distancia, 0, 230, 0, 100);
   
   Serial.print("Distancia ");
   Serial.print(nombre);
   Serial.print(" = ");
-  Serial.print(valor_distancia); 
-  Serial.println(" Mts3");
+  Serial.print(valor_distancia);
+  Serial.println(" cm");
+  Serial.print(valorPorcentaje);
+  Serial.println(" %");
 
   Firebase.setFloat(firebaseData, nodo + "/SalaA/" + nombre, valor_distancia);
 }
